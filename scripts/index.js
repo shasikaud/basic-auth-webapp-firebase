03223a12e8af91e2fbb76abc5db57fc72a567909
Shasika Udayanga <shasikaudayanga@gmail.com>
2020-07-22 00:15:25 +0530

    Commit 2

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+const guideList = document.querySelector('.guides');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelectorAll('.account-details');
+
+const setupUI = (user) => {
+  if (user){
+    // account info display
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `;
+      accountDetails.innerHTML = html;
+    });
+    // toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // hide ac info
+    accountDetails.innerHTML = '';
+    // toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
+
+//set up guides
+const setupGuides = (data) => {
+
+  if (data.length){
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      //console.log(guide);
+      const li = `
+        <li>
+          <div class="collapsible-header grtey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = '<h3> Login to view the content </h3>';
+  }
+};
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
